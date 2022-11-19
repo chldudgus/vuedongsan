e8a37fb25b48d9fa32cd613b57da071a25d65e1a
haleychoi <chldudgus617@gmail.com>
2022-11-19 17:05:07 +0900

    Create new Vue project - vuedongsan

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
+  <div>
+    <a>Home</a>
+    <a>Products</a>
+    <a>About</a>
+  </div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price3 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      // 여기다 데이터 보관
+      price1: 160,
+      price2: 50,
+      price3: 70,
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
